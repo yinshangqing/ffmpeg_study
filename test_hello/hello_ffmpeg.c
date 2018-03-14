6ec4b32d80bcf5b8bbc96ad1e1eebadffb7026a9
erric <erric@localhost.localdomain>
2018-03-14 07:42:52 -0400

    提交ffmpeg测试，查看版本

---
+++ test_hello/hello_ffmpeg.c
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: hello_ffmpeg.c
+	> Author: yinshangqing
+	> Mail: 841668821@qq.com 
+	> Created Time: Wed 14 Mar 2018 03:09:34 AM EDT
+ ************************************************************************/
+
+#include <stdio.h>
+  
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+ 
+  
+int main()
+{
+	printf("Hello FFmpeg!");
+	av_register_all();
+	unsigned int version = avcodec_version(); 
+	printf("version is: %d\n", version);
+
+	return 0;
+}
+
